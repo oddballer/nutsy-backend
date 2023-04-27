ba324cb9e5aaa6d78818139258df97ab921e13e0
TE-aj <aj.page@techelevator.com>
2023-04-27 09:45:09 -0600

    initial commit

---
+++ src/main/java/com/nutsy/nutsybackend/NutsyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nutsy.nutsybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutsyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutsyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutsy/nutsybackend/NutsyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutsy.nutsybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutsyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
