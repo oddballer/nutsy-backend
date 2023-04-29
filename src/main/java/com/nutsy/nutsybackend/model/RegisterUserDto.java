626e36239b72b35c3cf4f21804f706da633b1833
TE-aj <aj.page@techelevator.com>
2023-04-28 20:58:37 -0600

    new dto structure

--- src/main/java/com/nutsy/nutsybackend/model/RegisterUserDto.java
+++ src/main/java/com/nutsy/nutsybackend/model/RegisterUserDto.java
@@ -18,18 +18,6 @@ public class RegisterUserDto {
     private String password;
     @NotEmpty
     private String confirmPassword;
-    @NotEmpty
-    private String name;
-    private String address;
-    private String city;
-    @NotEmpty
-    @Pattern(regexp = "^(AL|AK|AR|AZ|CA|CO|CT|DC|DE|FL|GA|HI|IA|ID|IL|IN|KS|KY|LA|MA|MD|ME|MI|MN|MO|MS|MT|NC|ND|NE|NH|NJ|NM|NV|NY|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VA|VT|WA|WI|WV|WY)$",
-            message = "State code is invalid")
-    private String stateCode;
-    @NotEmpty
-    @Pattern(regexp = "^[0-9][0-9][0-9][0-9][0-9]$",
-            message = "ZIP must be 5 digits")
-    private String ZIP;
     @NotEmpty(message = "Please select a role for this user.")
     private String role;
 
@@ -63,45 +51,5 @@ public class RegisterUserDto {
 
     public void setRole(String role) {
         this.role = role;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStateCode() {
-        return stateCode;
-    }
-
-    public void setStateCode(String stateCode) {
-        this.stateCode = stateCode;
-    }
-
-    public String getZIP() {
-        return ZIP;
-    }
-
-    public void setZIP(String ZIP) {
-        this.ZIP = ZIP;
     }
 }
